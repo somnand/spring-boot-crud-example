1052773fe1bd568af6e51591cab5b3321a51918a
Somsurya Nanda <somsuryananda@gmail.com>
2026-03-09 00:05:03 +0530

    1. Introduced ResponseEntity in Controllers 2. Changes to logging using Slf4J and application.yml to control logging level

--- src/main/java/application/controller/UserRegistrationController.java
+++ src/main/java/application/controller/UserRegistrationController.java
@@ -1,24 +1,24 @@
 package application.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
+import application.model.UserEntity;
+import application.model.UserModel;
+import application.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import application.model.UserEntity;
-import application.model.UserModel;
-import application.model.VerificationToken;
-import application.service.UserService;
 import security.event.RegistrationCompleteEvent;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
 public class UserRegistrationController
 {
@@ -30,35 +30,52 @@ public class UserRegistrationController
     
     //POST Methods
     @PostMapping("/register")
-    public Long register(@RequestBody UserModel userModel, final HttpServletRequest request)
+    public ResponseEntity<UserEntity> register(@RequestBody UserModel userModel, final HttpServletRequest request)
     {
-	UserEntity savedUser = userService.userRegisterService(userModel);
-	publisher.publishEvent(new RegistrationCompleteEvent(savedUser, composeURL(request)));
-	return savedUser.getId();
+        UserEntity savedUser = userService.userRegisterService(userModel);
+        log.debug("User created and saved"+savedUser);
+        publisher.publishEvent(new RegistrationCompleteEvent(savedUser, composeURL(request)));
+        log.debug("Publishing Registration Completion Event");
+
+        if(savedUser!=null)
+            return new ResponseEntity(savedUser,HttpStatus.CREATED);
+        else
+            return new ResponseEntity(HttpStatus.EXPECTATION_FAILED);
     }
     
     private String composeURL(HttpServletRequest request)
     {
-	return "http://"+request.getServerName()+":"+request.getServerPort()+"/"+request.getContextPath();
+        StringBuilder url = new StringBuilder();
+        url.append("http://").append(request.getServerName()).append(":").append(request.getServerPort()).append("/").append(request.getContextPath());
+        log.debug("Composed URL : {}",url);
+	    return url.toString();
     }    
     
     @PostMapping("/registerUsers")
-    public List<Long> registerUsers(@RequestBody List<UserModel> userModels,final HttpServletRequest request)
+    public ResponseEntity<List<Long>> registerUsers(@RequestBody List<UserModel> userModels,final HttpServletRequest request)
     {
-	List<Long> savedIds = new ArrayList<>();
-	
-	for(UserModel userModel : userModels)
-	{
-	    savedIds.add(register(userModel,request));
-	}
-	
-	return savedIds;
+        List<Long> savedIds = new ArrayList<>();
+
+        try
+        {
+            for (UserModel userModel : userModels)
+            {
+                ResponseEntity<UserEntity> savedUserResponse = register(userModel, request);
+                savedIds.add(savedUserResponse.getBody().getId());
+            }
+        }
+        catch(RuntimeException re)
+        {
+            log.error("Error in saving multiple users ",re);
+            return new ResponseEntity<List<Long>>(HttpStatus.EXPECTATION_FAILED);
+        }
+        return new ResponseEntity<List<Long>>(savedIds,HttpStatus.CREATED);
     }
     
     //GET Methods
     @GetMapping("/allUsers")
     public List<UserEntity> getAllUsers()
     {
-	return userService.getAllUsers();
+        return userService.getAllUsers();
     }
 }
--- src/test/java/application/SpringBootCrudExampleApplicationTests.java
+++ src/test/java/application/SpringBootCrudExampleApplicationTests.java
@@ -19,7 +19,7 @@ class SpringBootCrudExampleApplicationTests {
 	// This is currently empty. It only tests if all the configurations are loaded properly.	
     }
 
-    //TODO Tets User save path
+    //TODO Test User save path
     //TODO Test verified tokens
     //TODO Test expiration
 }
